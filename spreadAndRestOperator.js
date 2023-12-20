d6b79db501c9c9e9ed917ba90083f009718fb80b
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-12-20 19:29:00 +0530

    spread , rest and string literls

---
+++ spreadAndRestOperator.js
@@ -0,0 +1,39 @@
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+// let arr3 = [7,8,9];
+
+// let res = [...arr1, ...arr2 , ...arr3];
+
+// console.log(res);
+
+
+// function sum(a ,b){
+//     return a+b;
+// }
+
+// function sum(a ,b ,c ,d ){
+//     return a+b+c + d ;
+// }
+
+// function sum(...args){
+
+//     let sum = 0 ;
+
+//     for(let i=0;i<args.length;i++){
+//         sum = sum + args[i];
+//     }
+
+//     return sum ;
+// }
+
+// console.log(sum(2,4 ,2 ,5,6,8,3,3,4,5))
+
+let Name = "saurabh";
+let age = 23 ;
+let skilss = "html , css ";
+let para = "My name is " + Name + " my age is " + age + "my skilss are " + skilss;
+
+let para2 = `My name is ${Name} , My age is ${age} , My skilss are ${skilss}`
+
+console.log(para2);
