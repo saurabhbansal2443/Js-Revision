1c405d3b6e7e1e91fce43354b2c1ae4b601515b5
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-12-20 17:21:35 +0530

    callbacks

--- newBatch/callback/sandwich.js
+++ newBatch/callback/sandwich.js
@@ -1,20 +1,44 @@
-
 let raw = "bread";
 
-console.log("  we have started here ")
-
-function cuttingBread(raw , callback1){
-    setTimeout(function(){
-        console.log("Bread is cuted ");
-        callback1();
-    },2000);
+function cuttingBread(raw, callback1) {
+    setTimeout(function () {
+        // console.log("Bread is cuted ");
+        let bread = raw;
+        callback1(bread);
+    }, 2000);
 }
 
-function callback1(){
-    console.log("Bread is now ready ");
+function addsauces(bread, callback2) {
+    setTimeout(() => {
+        let base = bread + " with sauces ";
+        callback2(base);
+    }, 2000);
+}
+
+function addTopings(base, callback3) {
+    setTimeout(() => {
+        let sandwich = base + " and topings "
+        callback3(sandwich);
+    }, 2000);
+}
+
+function bakeSandwich(sandwich, callback4) {
+    setTimeout(() => {
+        let readySandwich = sandwich + " and baked also ";
+        callback4(readySandwich);
+    }, 2000);
 }
 
 
-cuttingBread(raw , callback1);
-
-console.log("  we have ended here ")
+cuttingBread(raw, function (bread) { // first process is called with raw material 
+    console.log(bread);
+    addsauces(bread, function (base) {
+        console.log(base);
+        addTopings(base, function (sandwich) {
+            console.log(sandwich);
+            bakeSandwich(sandwich, function (readySandwich) {
+                console.log(readySandwich);
+            })
+        })
+    })
+})
