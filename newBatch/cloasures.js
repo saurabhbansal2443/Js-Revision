386cab120120f6613f73e13d5dc31b8e7bbf2618
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-12-21 19:01:07 +0530

    closures

---
+++ newBatch/cloasures.js
@@ -0,0 +1,31 @@
+
+function counter(){
+let count = 0 ;
+
+return  {
+    inc : function increment(){
+        count++;
+        console.log(count);
+    },
+    dec : function decrement(){
+        count--;
+        console.log(count);
+    }
+}
+
+
+}
+
+let ans = counter();
+
+let increment  = ans.inc;
+let decrement = ans.dec;
+
+increment();
+
+decrement();
+
+count = 50;
+
+increment()
+
