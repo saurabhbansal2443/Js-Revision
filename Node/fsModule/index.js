1ba794f3cab0dac1d7e0fd8c1f6317c84f7d6e1e
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-12-27 18:50:35 +0530

    fs module completed

--- Node/fsModule/index.js
+++ Node/fsModule/index.js
@@ -1,5 +1,19 @@
 import fs from 'fs';
 
 
-// fs.mkdirSync("./newFolder")
-fs.rmdirSync("./newFolder")
+// fs.mkdirSync("./newFolder") // to create folder
+//fs.rmdirSync("./newFolder") // to remove folder 
+
+// fs.writeFileSync("file1.txt","I am the text of file one ");// creating a file with some text 
+
+// fs.writeFileSync("file2.pdf","i am the text of file2");
+
+// let data = fs.readFileSync("file1.txt");  // reading the text of a file 
+
+// console.log(data.toString());
+
+// fs.unlinkSync("./file2.pdf") // delete a file 
+
+// fs.appendFileSync("./file1.txt" , "I am the content which is added ");
+
+fs.writeFileSync("./file1.txt","i am updated content ")
