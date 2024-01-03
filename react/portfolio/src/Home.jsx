5c218de91de4ba42fa0c2957834760e9b1a23b6e
Saurabh Bansal <saurabhbansal2443@gmail.com>
2024-01-03 19:06:44 +0530

    home page

--- react/portfolio/src/Home.jsx
+++ react/portfolio/src/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import "./HomeStyle.css"
 
 const Home = () => {
   return (
-    <div>This is a home component </div>
+    <div className='mainContainer'>
+      <div className="textPart">
+        <div className="textIn"> <h1>Hey, I am <br />   Saurabh Bansal  </h1>
+        <h1> I am a Software Developer </h1>
+        </div>
+        <div className="aboutMe">
+        <button type="button" className="btn btn-warning">AboutMe</button>
+        </div>
+        <div className="socialCircle">
+        <img className="logo"src="../assets/github.svg"></img>
+        <img className="logo"src="../assets/instagram (1).svg"></img>
+        <img className="logo"src="../assets/linkedin.svg"></img>
+        <img className="logo"src="../assets/telegram.svg"></img>
+        <img className="logo"src="../assets/twitter.svg"></img>
+        </div>
+      </div>
+      <div className="avatarPart">
+        <img src="../assets/avatar.jpeg" className="avatar"></img>
+      </div>
+    </div>
   )
 }
 
--- react/portfolio/src/Navbar.jsx
+++ react/portfolio/src/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Saurabh Bansal </Link>
+          <Link className="navbar-brand" to="/">Saurabh </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
