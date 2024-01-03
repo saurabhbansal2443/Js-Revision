5e5a9cd295b6f0012dfedd53e7d600aa30174a1c
Saurabh Bansal <saurabhbansal2443@gmail.com>
2024-01-03 19:21:45 +0530

    react cdn

--- react/portfolio/src/Home.jsx
+++ react/portfolio/src/Home.jsx
@@ -4,19 +4,25 @@ import "./HomeStyle.css"
 const Home = () => {
   return (
     <div className='mainContainer'>
-      <div className="textPart">
-        <div className="textIn"> <h1>Hey, I am <br />   Saurabh Bansal  </h1>
-        <h1> I am a Software Developer </h1>
-        </div>
+      <div className="textPart" style={{fontSize:"2rem" , fontWeight:"500"}}>
+        <main class="container">
+          <p>Hello 👋 I'm <br />
+          Saurabh Bansal</p>
+          <section class="animation">
+            <div class="first"><div> </div></div>
+            <div class="second"><div>Web Developer</div></div>
+            <div class="third"><div>Systems Engineer</div></div>
+          </section>
+        </main>
         <div className="aboutMe">
-        <button type="button" className="btn btn-warning">AboutMe</button>
+          <button type="button" className="btn btn-warning btn-lg glow">AboutMe</button>
         </div>
         <div className="socialCircle">
-        <img className="logo"src="../assets/github.svg"></img>
-        <img className="logo"src="../assets/instagram (1).svg"></img>
-        <img className="logo"src="../assets/linkedin.svg"></img>
-        <img className="logo"src="../assets/telegram.svg"></img>
-        <img className="logo"src="../assets/twitter.svg"></img>
+          <img className="logo" src="../assets/github.svg"></img>
+          <img className="logo" src="../assets/instagram (1).svg"></img>
+          <img className="logo" src="../assets/linkedin.svg"></img>
+          <img className="logo" src="../assets/telegram.svg"></img>
+          <img className="logo" src="../assets/twitter.svg"></img>
         </div>
       </div>
       <div className="avatarPart">
