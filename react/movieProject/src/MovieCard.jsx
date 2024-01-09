02ffe0c62f53c351430168e57e4ec73f3ca637e1
Saurabh Bansal <saurabhbansal2443@gmail.com>
2024-01-09 08:10:38 +0530

    movie card

--- react/movieProject/src/MovieCard.jsx
+++ react/movieProject/src/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieCard = () => {
   return (
     <div className=' border-4 border-red-950 h-3/4 w-1/6'>
-      <button className="" ></button>
+      <button className="" > Add to Fav </button>
     </div>
   )
 }
